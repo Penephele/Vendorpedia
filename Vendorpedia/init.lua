12e9bf634161fae4a68a23e0dd7de55fb505dee2
Penephele <penephele@gmail.com>
2026-02-23 05:45:31 -0500

    Add files via upload

---
+++ Vendorpedia/init.lua
@@ -0,0 +1,25 @@
+
+VPMerchants = VPMerchants or {}
+VPItems = VPItems or {}
+
+VPVersion = {
+    major = 0,
+    minor = 0,
+    patch = 1
+}
+
+VPFactions = {
+  "The Aldor",
+  "Darnassus",
+  "Exodar",
+  "Gilneas",
+  "Gnomeregan",
+  "Ironforge",
+  "Kirin Tor",
+  "The Scryers",
+  "Stormwind",
+  "Tushui Pandaren",
+}
+
+SLASH_VENDORPEDIA1 = "/vpa"
+SLASH_VENDORPEDIA2 = "/vendorpedia"
